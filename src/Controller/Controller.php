37edea0edd886345877d065cd41d4159099f62ed
mbouc <74280078+matthewbouc@users.noreply.github.com>
2022-08-08 09:52:27 -0500

    Initial setup of Dockerfile, endpoints, and logger.

---
+++ src/Controller/Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractController
+{
+    #[Route('/')]
+    #[Route('/health')]
+    public function health(): Response {
+        return new Response();
+    }
+
+    #[Route('/log/{name}')]
+    public function log(string $name, LoggerInterface $logger): Response {
+        // See these in /var/log/ of your project root
+        $logger->info("Hello, $name");
+        return $this->json([
+            'success' => true,
+            'name' => $name
+        ]);
+    }
+}
